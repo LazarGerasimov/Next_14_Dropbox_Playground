3c35f8dd5529d830a67e26d99b3852fddd2f7f15
Laz <lazgerasimov@gmail.com>
2023-12-12 16:20:48 +0200

    complete Homepage: - add Dropbox video - make fully responsive layout - add Dashboard redirection link - add styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,40 @@
+import { ArrowRight } from "lucide-react";
+import Link from "next/link";
 
 
 export default function Home() {
   return (
     <main className="">
-      <h1>Dropbox 2.0 </h1>
+      <div className="flex flex-col lg:flex-row items-center bg-[#1E1919] dark:bg-slate-800">
+        <div className="p-10 flex flex-col bg-[#2B2929] dark:bg-slate-800 text-white space-y-5">
+          <h1 className="text-5xl font-bold">
+            Welcome to Dropbox Playground. <br />
+            <br />
+            Storing everything for you and your business. All in one place.
+          </h1>
+
+          <p className="pb-20">
+            Enhance your personal storage with Dropbox, offering a simple and efficient way to upload, organize and access files from anywhere.
+            Securely store important documents and media and experience the convenience of easy file management and sharing in one centralized solution.
+          </p>
+
+          <Link href='/dashboard' className="flex cursor-pointer bg-blue-500 p-5 w-fit">
+            Try it for free!
+            <ArrowRight className="ml-3" />
+          </Link>
+        </div>
+
+        <div className="bg-[#1E1919] dark:bg-slate-800 h-full p-10">
+          <video autoPlay loop muted className="rounded-lg">
+            <source
+              src="https://aem.dropbox.com/cms/content/dam/dropbox/warp/en-us/overview/lp-header-graphite200-1920x1080.mp4"
+              type="video/mp4"
+            />
+            Your browser does not support the video tag.
+          </video>
+        </div>
+
+      </div>
     </main>
   )
 }
