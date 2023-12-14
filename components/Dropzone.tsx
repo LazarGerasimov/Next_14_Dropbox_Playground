c8d7611dcd3c348a68bf9574a12aa43824270eaf
Laz <lazgerasimov@gmail.com>
2023-12-14 09:14:18 +0200

    setup file uploading scaffold

--- components/Dropzone.tsx
+++ components/Dropzone.tsx
@@ -1,9 +1,40 @@
 "use client";
 
 import { cn } from '@/lib/utils';
+import { useUser } from '@clerk/nextjs';
+import { useState } from 'react';
 import DropzoneComponent from 'react-dropzone'
 
 const Dropzone = () => {
+
+    const [loading, setLoading] = useState(false);
+    const { isLoaded, isSignedIn, user } = useUser();
+
+    const onDrop = (acceptedFiles: File[]) => {
+        acceptedFiles.forEach(file => {
+            const reader = new FileReader();
+
+            reader.onabort = () => console.log("file reading was aborted");
+            reader.onerror = () => console.log("file reading has failed");
+
+            reader.onload = async () => {
+                await uploadPost(file);
+            };
+
+            reader.readAsArrayBuffer(file);
+        })
+    };
+
+    const uploadPost = async (selectedFile: File) => {
+        if (loading) return;
+        if (!user) return;
+
+        setLoading(true);
+
+        // do what needs to be done
+
+        setLoading(false);
+    }
 
     const maxSize = 20971520;
 
@@ -22,13 +53,13 @@ const Dropzone = () => {
                 return (
                     <section className='m-4'>
                         <div {...getRootProps()}
-                        className={cn(
-                            "w-full h-52 flex justify-center items-center p-5 border border-dashed rounded-lg text-center",
-                            isDragActive
-                            ? "bg-[#035FFE] text-white animate-pulse"
-                            : "bg-slate-100/50 dark:bg-slate-800/80 text-slate-400"
+                            className={cn(
+                                "w-full h-52 flex justify-center items-center p-5 border border-dashed rounded-lg text-center",
+                                isDragActive
+                                    ? "bg-[#035FFE] text-white animate-pulse"
+                                    : "bg-slate-100/50 dark:bg-slate-800/80 text-slate-400"
 
-                        )}
+                            )}
                         >
                             <input  {...getInputProps()} />
                             {!isDragActive && "Click here or drop a file to upload!"}
