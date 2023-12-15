32570783d3925e5f6918cbf86660ad1f0f463ed2
Laz <lazgerasimov@gmail.com>
2023-12-15 19:32:45 +0200

    setup firebase & cloud firestore

---
+++ firebase.ts
@@ -0,0 +1,16 @@
+import { getApp, getApps, initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAkG0-Fe_gPN4-asXKvRfDhF1njTFG243Y",
+    authDomain: "dropbox-clone-df98b.firebaseapp.com",
+    projectId: "dropbox-clone-df98b",
+    storageBucket: "dropbox-clone-df98b.appspot.com",
+    messagingSenderId: "521949216516",
+    appId: "1:521949216516:web:4dde1e625a302d7211da51"
+};
+
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const storage = getStorage(app);
