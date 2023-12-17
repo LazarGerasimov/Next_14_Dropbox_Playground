e2b1dfeb4eefcddca9ecd8284c0719562c4be8d7
Laz <lazgerasimov@gmail.com>
2023-12-17 08:27:28 +0200

    create pre-loading data table scaffold

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,13 +1,42 @@
 import { auth } from "@clerk/nextjs"
 import Dropzone from "@/components/Dropzone";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "@/firebase";
+import { FileType } from "@/typings";
+import TableWrapper from "@/components/table/TableWrapper";
 
-const Dashboard = () => {
+// top level async can only be done on server components
+async function Dashboard() {
 
   const { userId, } = auth();
 
+  const docResults = await getDocs(collection(db, "users", userId!, "files"));
+
+  const skeletonFiles: FileType[] = docResults.docs.map(doc => ({
+    id: doc.id,
+    filename: doc.data().filename || doc.id,
+    timestamp: new Date(doc.data().timestamp?.seconds * 1000) || undefined,
+    fullName: doc.data().fullName,
+    downloadURL: doc.data().downloadURL,
+    type: doc.data().type,
+    size: doc.data().size
+  }));
+
+  console.log(skeletonFiles);
+
   return (
-    <div>
+    <div className="border-t">
       <Dropzone />
+
+      <section className="container space-y-5">
+        <h2 className="font-bold">All Files</h2>
+        <div>
+          {/* Table wrapper */}
+          <TableWrapper
+            skeletonFiles={skeletonFiles}
+          />
+        </div>
+      </section>
     </div>
   )
 }
---
+++ components/table/TableWrapper.tsx
@@ -0,0 +1,12 @@
+import { FileType } from '@/typings'
+import React from 'react'
+
+const TableWrapper = (
+    { skeletonFiles }: { skeletonFiles: FileType[] }
+) => {
+    return (
+        <div>TableWrapper</div>
+    )
+}
+
+export default TableWrapper
